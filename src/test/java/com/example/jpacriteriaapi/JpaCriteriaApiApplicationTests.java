ab2d53cfe22670c9bad6f5de0c1f89b04d4aa391
Only-Java <javabackendeveloper@gmail.com>
2024-09-23 18:08:27 +0500

    add git

---
+++ src/test/java/com/example/jpacriteriaapi/JpaCriteriaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpacriteriaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaCriteriaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
